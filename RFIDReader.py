b9536da43dedcb9050457e74005784bfff48c270
TheLonelyGod <Shy@shyruparel.com>
2013-01-13 21:23:09 -0500

    Formated the output string to remove '/n'

--- RFIDReader.py
+++ RFIDReader.py
@@ -7,6 +7,7 @@ ser = serial.Serial(port ,speed)
 
 while True:
 	data = ser.readline()
-	if data != None:
-		print data
+	if data != None:	
+		print 'Output: ' + ser.readline().rstrip()
+		
 ser.close()
