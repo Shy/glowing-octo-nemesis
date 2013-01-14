6260f0b7c3da996d9f5f580fdbfea0c9508d083c
TheLonelyGod <Shy@shyruparel.com>
2013-01-14 14:18:42 -0500

    Removed gedit temp file

--- RFIDReader.py
+++ RFIDReader.py
@@ -1,13 +1,21 @@
 import serial
 
-port = '/dev/ttyUSB0'
-speed = 2400 
+def IDCheck(data):
+	if data== '4E009C33FA':
+		print 'Shys wallet'
+	elif data == '5900285E23':
+		print 'Keychain'
 
-ser = serial.Serial(port ,speed)
+port = '/dev/ttyUSB0'
+speed = 2400
+
+ser = serial.Serial(port, speed)
 
 while True:
 	data = ser.readline()
-	if data != None:	
-		print 'Output: ' + ser.readline().rstrip()
-		
+	if data != None:
+		data = data.rstrip()
+		print 'Output: ' + data
+		IDCheck(data)
+		ser.flushInput()
 ser.close()
